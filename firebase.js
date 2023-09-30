2ce6605892a2c98589fb0949bfb46d07f8afc440
Magalhaes-Cintra <132522385+Magalhaes-Cintra@users.noreply.github.com>
2023-09-29 21:57:57 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyC-1wP8IQbpJsfQI91EHT0JGI73aP-5yXk',
+  authDomain: 'pizzariapivas.firebaseapp.com',
+  projectId: 'pizzariapivas',
+  storageBucket: 'pizzariapivas.appspot.com',
+  appId: '1:974117730810:web:73067a261b028e8e77cc43',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
